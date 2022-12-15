c30f39c47a754bd32432e386eeee28a103e50ec4
yun <kilicapeto@gmail.com>
2022-12-16 02:36:41 +0800

    fix: get value from aws

--- src/devops/build-ecr.sh
+++ src/devops/build-ecr.sh
@@ -4,6 +4,8 @@ set -x
 # build docker iamge
 docker build -t imbee .
 
+ECR_IMBEE_REPO_URL=$(aws --region=ap-northeast-1 ssm get-parameter --name "/imbee/infra/notification-ecr" --with-decryption --output text --query Parameter.Value)
+
 # login to ecr awscliv2
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin $ECR_IMBEE_REPO_URL
 
